2092c0947aa58e7a3da42d2d02a9d558cfdf8b13
ChrisShadow <chrisdanielbenee@gmail.com>
2023-07-12 20:44:35 -0400

    following with lugartrabajo

--- php/RRHH.Cargo.V2/model/listahorario.php
+++ php/RRHH.Cargo.V2/model/listahorario.php
@@ -2,6 +2,7 @@
 namespace Model;
 
 use Model\LugarTrabajo;
+use Model\Horario;
 
 #region doc
 https: //www.php.net/manual/en/class.ds-map.php#:~:text=A%20Map%20is%20a%20sequential,map%20using%20the%20same%20key.
@@ -10,16 +11,17 @@ https: //www.php.net/manual/en/class.ds-map.php#:~:text=A%20Map%20is%20a%20seque
 class ListaHorario
 {
     #region att
-    private static $MapaHorario = (object) []; //[{Lugar trabajo:Horario}]
-    private static $ArrayHorario = array(); //posición y el objeto instanciado horario
+    private static $MapaHorario = []; //[{Lugar trabajo:Horario}]
+    private static $ArrayHorario; //posición y el objeto instanciado horario
     private LugarTrabajo $lugarTrabajo;
     #endregion
 
     #region const
     function __construct(
-        LugarTrabajo $lugarTrabajo
+
     ) {
-        $this->lugarTrabajo = $lugarTrabajo;
+        self::$MapaHorario = (object) [];
+        self::$ArrayHorario = array();
     }
     #endregion
 
@@ -38,11 +40,33 @@ class ListaHorario
     #endregion
 
     #region methods
+    //Creating and setting the new map
+    //Just for the first time
+    public function set_key_lista_horario()
+    {
+        self::$MapaHorario = [$this->lugarTrabajo => self::$ArrayHorario]; //[{Lugar trabajo:Horario}]
+    }
+
+    //Adding new elements to horario list which is the value in the map
+    public function set_value_horario(Horario $horario)
+    {
+        if (self::$ArrayHorario == array())
+            $index = 0;
+        self::$ArrayHorario[$index] = $horario;
+        $index++;
+    }
+
+    /*Next act, do a test with an aux class regardin listahorario for looking which goes firs= either set_key_lista_horario or set_value_horario
+    Then the other functions: delete, seek/edit. Lastly show_data
+    */
+
     public function show_data()
     {
         $response = "Lista horario: [Lugar de trabajo: " . $this->get_lugarTrabajo() . "]";
         return json_encode($response);
     }
+
+
     #endregion
 }
 
