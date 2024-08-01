4a34299487e4c22c7706a3082812cf48a2256cbc
ChrisShadow <chrisdanielbenee@gmail.com>
2024-08-01 18:19:02 -0400

    testing back&front-end service

--- php/crud-example/routes/web.php
+++ php/crud-example/routes/web.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\TaskViewController;
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('tasks', TaskViewController::class);
+
+
